1df254bee3dbc36f5fa3818987d3d56b66ab61c1
tzn <137900114@qq.com>
2022-09-06 12:15:40 +0800

    upgrade powerCon

--- template/io.c
+++ template/io.c
@@ -26,6 +26,29 @@ void print_u8(uint64_t n){
 
 
 void test_5g(){
-    const char* msg = "Blocked by america.Please buy HuaWei to enable 5g";
+    const char* msg = "Blocked by America.Please buy HuaWei to enable 5g";
     printf("%s\n",msg);
+}
+
+void powerCon(uint64_t na,int offset){
+    int a[8],h = -1;
+    for(int b = 0;b < 8;b++){
+        int offset = (7 - b) * 8;
+        a[b] = (n & (0xffll << offset) ) >> offset;
+        if(a[b] != 0 && h < 0){
+            h = b;
+        }
+    }
+
+    static int keyboard[26] = {0};
+    for(int i = 0;i < 8 - h;i++){
+        int force = a[i + h];
+        keyboard[offset + i] = force;
+    }
+    
+    printf("[%d",keyboard[0]);
+    for(int i = 1;i < 26;i++){
+        printf(",%d",keyboard[i]);
+    }
+    printf("]\n");
 }
